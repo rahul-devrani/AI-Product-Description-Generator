2d7d799f24c2e661aeb6e1f0dab0ad551eeb50ab
Rahul Devrani <devranirahul1@gmail.com>
2026-08-01 14:09:57 +0530

    fix: support Firebase secret file on Render

--- backend/app/firebase_admin.py
+++ backend/app/firebase_admin.py
@@ -1,12 +1,17 @@
+import os
 import firebase_admin
 
 from firebase_admin import credentials
-
 from firebase_admin import auth
 
 
-cred = credentials.Certificate(
-    "app/firebase/firebase-key.json"
-)
+if os.path.exists("/etc/secrets/firebase-key.json"):
+    firebase_path = "/etc/secrets/firebase-key.json"
+else:
+    firebase_path = "app/firebase/firebase-key.json"
 
-firebase_admin.initialize_app(cred)
+
+cred = credentials.Certificate(firebase_path)
+
+if not firebase_admin._apps:
+    firebase_admin.initialize_app(cred)
